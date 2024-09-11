01e45cf7dd331464bec1be5fe3153ff634df9389
Jonathan Nappi <jnappi@uptycs.com>
2024-09-11 16:35:39 -0400

    use sudo mv/chmod

--- src/scripts/install-uptycs-cli.sh
+++ src/scripts/install-uptycs-cli.sh
@@ -25,7 +25,5 @@ echo "downloading https://downloads.uptycs.io/uptycs-cli/${BINARY_NAME}.tar.gz"
 
 curl -o "${DOWNLOAD_DIR}/${BINARY_NAME}.tar.gz" "https://downloads.uptycs.io/uptycs-cli/${BINARY_NAME}.tar.gz"
 tar -xf "${DOWNLOAD_DIR}/${BINARY_NAME}.tar.gz" --directory "${DOWNLOAD_DIR}/"
-whoami
-mkdir -p "${INSTALL_DIR}"
-mv "${DOWNLOAD_DIR}/bin/uptycs-cli" "${INSTALL_DIR}/uptycs-cli"
-chmod +x "${INSTALL_DIR}/uptycs-cli"
+sudo mv "${DOWNLOAD_DIR}/bin/uptycs-cli" "${INSTALL_DIR}/uptycs-cli"
+sudo chmod +x "${INSTALL_DIR}/uptycs-cli"
